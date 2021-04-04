57cf0f73ff2381cedc19fa118e718cb7e1de332c
Mikedeejay2 <58639173+Mikedeejay2@users.noreply.github.com>
2021-04-04 14:42:03 -0400

    SleepIn

---
+++ src/com/mikedeejay2/codingbatsolutions/solutions/warmup1/SleepIn.java
@@ -0,0 +1,47 @@
+package com.mikedeejay2.codingbatsolutions.solutions.warmup1;
+
+import com.mikedeejay2.codingbatsolutions.internal.CodingBatSolution;
+import com.mikedeejay2.codingbatsolutions.internal.annotations.Inputs;
+import com.mikedeejay2.codingbatsolutions.internal.annotations.Results;
+import com.mikedeejay2.codingbatsolutions.internal.annotations.Solution;
+
+public class SleepIn implements CodingBatSolution {
+
+
+    /**
+     * The parameter weekday is true if it is a weekday, and the parameter vacation is true if we are on vacation.
+     * We sleep in if it is not a weekday or we're on vacation. Return true if we sleep in.
+     *
+     * @param weekday  boolean representing the weekday
+     * @param vacation boolean representing the vacation
+     * @return whether we sleep in or not
+     */
+    @Solution
+    public boolean sleepIn(boolean weekday, boolean vacation) {
+        return !weekday || vacation;
+    }
+
+
+
+
+
+    @Inputs
+    public Object[][] inputs() {
+        return new Object[][] {
+                {false, false},
+                {true, false},
+                {false, true},
+                {true, true},
+        };
+    }
+
+    @Results
+    public Object[] results() {
+        return new Object[] {
+                true,
+                false,
+                true,
+                true
+        };
+    }
+}
