df2ae502845e6cf73ab049c8bc42d20718639e3d
Mikedeejay2 <58639173+Mikedeejay2@users.noreply.github.com>
2021-04-04 14:45:50 -0400

    MonkeyTrouble

---
+++ src/com/mikedeejay2/codingbatsolutions/solutions/warmup1/MonkeyTrouble.java
@@ -0,0 +1,44 @@
+package com.mikedeejay2.codingbatsolutions.solutions.warmup1;
+
+import com.mikedeejay2.codingbatsolutions.internal.CodingBatSolution;
+import com.mikedeejay2.codingbatsolutions.internal.annotations.Inputs;
+import com.mikedeejay2.codingbatsolutions.internal.annotations.Results;
+import com.mikedeejay2.codingbatsolutions.internal.annotations.Solution;
+
+public class MonkeyTrouble implements CodingBatSolution {
+
+
+
+    /**
+     * We have two monkeys, a and b, and the parameters aSmile and bSmile indicate if each is smiling. We are in
+     * trouble if they are both smiling or if neither of them is smiling. Return true if we are in trouble.
+     */
+    @Solution
+    public boolean monkeyTrouble(boolean aSmile, boolean bSmile) {
+        return (aSmile && bSmile) || (!aSmile && !bSmile);
+    }
+
+
+
+
+
+    @Inputs
+    public Object[][] inputs() {
+        return new Object[][] {
+                {true, true},
+                {false, false},
+                {true, false},
+                {false, true},
+        };
+    }
+
+    @Results
+    public Object[] results() {
+        return new Object[] {
+                true,
+                true,
+                false,
+                false,
+        };
+    }
+}
