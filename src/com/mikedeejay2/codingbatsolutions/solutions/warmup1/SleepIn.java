89162100233d95588c1e26a9d38af0f1aa07eaa9
Mikedeejay2 <58639173+Mikedeejay2@users.noreply.github.com>
2021-04-04 14:42:55 -0400

    Add a single space

--- src/com/mikedeejay2/codingbatsolutions/solutions/warmup1/SleepIn.java
+++ src/com/mikedeejay2/codingbatsolutions/solutions/warmup1/SleepIn.java
@@ -8,6 +8,7 @@ import com.mikedeejay2.codingbatsolutions.internal.annotations.Solution;
 public class SleepIn implements CodingBatSolution {
 
 
+
     /**
      * The parameter weekday is true if it is a weekday, and the parameter vacation is true if we are on vacation.
      * We sleep in if it is not a weekday or we're on vacation. Return true if we sleep in.
