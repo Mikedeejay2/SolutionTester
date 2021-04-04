05668df49386274d64087fa969f113bb8aed1a4a
Mikedeejay2 <58639173+Mikedeejay2@users.noreply.github.com>
2021-04-04 14:58:33 -0400

    Diff21

---
+++ src/com/mikedeejay2/codingbatsolutions/solutions/warmup1/Diff21.java
@@ -0,0 +1,62 @@
+package com.mikedeejay2.codingbatsolutions.solutions.warmup1;
+
+import com.mikedeejay2.codingbatsolutions.internal.CodingBatSolution;
+import com.mikedeejay2.codingbatsolutions.internal.annotations.Inputs;
+import com.mikedeejay2.codingbatsolutions.internal.annotations.Results;
+import com.mikedeejay2.codingbatsolutions.internal.annotations.Solution;
+
+public class Diff21 implements CodingBatSolution {
+
+
+
+    /*
+     * Given an int n, return the absolute difference between n and 21, except return double the absolute difference
+     * if n is over 21.
+     */
+    @Solution
+    public int diff21(int n) {
+        return Math.abs(n > 21 ? (21 - n) * 2 : 21 - n);
+    }
+
+
+
+
+
+
+
+    @Inputs
+    public Object[] inputs() {
+        return new Object[] {
+                19,
+                10,
+                21,
+                22,
+                25,
+                30,
+                0,
+                1,
+                2,
+                -1,
+                -2,
+                50,
+        };
+    }
+
+    @Results
+    public Object[] results() {
+        return new Object[] {
+                2,
+                11,
+                0,
+                2,
+                8,
+                18,
+                21,
+                20,
+                19,
+                22,
+                23,
+                58,
+        };
+    }
+}
