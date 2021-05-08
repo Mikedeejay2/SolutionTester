88540f5a5005690c8934639aacee9f6a9c48fea3
Mikedeejay2 <58639173+Mikedeejay2@users.noreply.github.com>
2021-05-08 13:17:00 -0400

    BackAround

---
+++ src/com/mikedeejay2/codingbatsolutions/solutions/warmup1/BackAround.java
@@ -0,0 +1,52 @@
+package com.mikedeejay2.codingbatsolutions.solutions.warmup1;
+
+import com.mikedeejay2.codingbatsolutions.internal.CodingBatSolution;
+import com.mikedeejay2.codingbatsolutions.internal.annotations.Inputs;
+import com.mikedeejay2.codingbatsolutions.internal.annotations.Results;
+import com.mikedeejay2.codingbatsolutions.internal.annotations.Solution;
+
+public class BackAround implements CodingBatSolution {
+
+
+
+/*
+ * Given a string, take the last char and return a new string with the last char added at the front and back, so "cat" yields "tcatt". The original string will be length 1 or more.
+ *
+ *
+ * backAround("cat") → "tcatt"
+ * backAround("Hello") → "oHelloo"
+ * backAround("a") → "aaa"
+ */
+@Solution
+public String backAround(String str) {
+    return str.isEmpty() ? str : str.charAt(str.length() - 1) + str + str.charAt(str.length() - 1);
+}
+
+
+
+
+
+    @Inputs
+    public Object[] inputs() {
+        return new Object[] {
+            "cat",
+            "Hello",
+            "a",
+            "abc",
+            "read",
+            "boo",
+        };
+    }
+
+    @Results
+    public Object[] results() {
+        return new Object[] {
+            "tcatt",
+            "oHelloo",
+            "aaa",
+            "cabcc",
+            "dreadd",
+            "obooo",
+        };
+    }
+}
