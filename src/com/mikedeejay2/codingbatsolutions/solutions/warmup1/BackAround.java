35fc2fc57a4e5763cd7c7976753f2258cdbf8ab6
Mikedeejay2 <58639173+Mikedeejay2@users.noreply.github.com>
2021-05-08 13:51:12 -0400

    Wrap comment text

--- src/com/mikedeejay2/codingbatsolutions/solutions/warmup1/BackAround.java
+++ src/com/mikedeejay2/codingbatsolutions/solutions/warmup1/BackAround.java
@@ -10,7 +10,8 @@ public class BackAround implements CodingBatSolution {
 
 
 /*
- * Given a string, take the last char and return a new string with the last char added at the front and back, so "cat" yields "tcatt". The original string will be length 1 or more.
+ * Given a string, take the last char and return a new string with the last char added at the front and back, so "cat"
+ * yields "tcatt". The original string will be length 1 or more.
  *
  *
  * backAround("cat") → "tcatt"
--- src/com/mikedeejay2/codingbatsolutions/solutions/warmup1/Or35.java
+++ src/com/mikedeejay2/codingbatsolutions/solutions/warmup1/Or35.java
@@ -10,7 +10,8 @@ public class Or35 implements CodingBatSolution {
 
 
 /*
- * Return true if the given non-negative number is a multiple of 3 or a multiple of 5. Use the % "mod" operator -- see Introduction to Mod
+ * Return true if the given non-negative number is a multiple of 3 or a multiple of 5. Use the % "mod" operator -- see
+ * Introduction to Mod
  *
  *
  * or35(3) → true
