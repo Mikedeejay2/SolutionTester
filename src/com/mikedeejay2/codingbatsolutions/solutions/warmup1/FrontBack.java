f74a8efde4966bc4557afcab2854311dd0aba340
Mikedeejay2 <58639173+Mikedeejay2@users.noreply.github.com>
2021-05-08 12:54:59 -0400

    FrontBack

---
+++ src/com/mikedeejay2/codingbatsolutions/solutions/warmup1/FrontBack.java
@@ -0,0 +1,56 @@
+package com.mikedeejay2.codingbatsolutions.solutions.warmup1;
+
+import com.mikedeejay2.codingbatsolutions.internal.CodingBatSolution;
+import com.mikedeejay2.codingbatsolutions.internal.annotations.Inputs;
+import com.mikedeejay2.codingbatsolutions.internal.annotations.Results;
+import com.mikedeejay2.codingbatsolutions.internal.annotations.Solution;
+
+public class FrontBack implements CodingBatSolution {
+
+
+
+/*
+ * Given a string, return a new string where the first and last chars have been exchanged.
+ *
+ *
+ * frontBack("code") → "eodc"
+ * frontBack("a") → "a"
+ * frontBack("ab") → "ba"
+ */
+@Solution
+public String frontBack(String str) {
+    return str.length() < 2 ? str : str.charAt(str.length() - 1) + str.substring(1, str.length() - 1) + str.charAt(0);
+}
+
+
+
+
+
+    @Inputs
+    public Object[] inputs() {
+        return new Object[] {
+            "code",
+            "a",
+            "ab",
+            "abc",
+            "",
+            "Chocolate",
+            "aavJ",
+            "hello",
+        };
+    }
+
+    @Results
+    public Object[] results() {
+        return new Object[] {
+            "eodc",
+            "a",
+            "ba",
+            "cba",
+            "",
+            "ehocolatC",
+            "Java",
+            "oellh",
+        };
+    }
+}
