42af9bf473ac6d27ca8486745bd4a45a327f9313
Mikedeejay2 <58639173+Mikedeejay2@users.noreply.github.com>
2021-05-09 16:07:40 -0400

    MixStart

---
+++ src/com/mikedeejay2/codingbatsolutions/solutions/warmup1/MixStart.java
@@ -0,0 +1,54 @@
+package com.mikedeejay2.codingbatsolutions.solutions.warmup1;
+
+import com.mikedeejay2.codingbatsolutions.internal.CodingBatSolution;
+import com.mikedeejay2.codingbatsolutions.internal.annotations.Inputs;
+import com.mikedeejay2.codingbatsolutions.internal.annotations.Results;
+import com.mikedeejay2.codingbatsolutions.internal.annotations.Solution;
+
+public class MixStart implements CodingBatSolution {
+
+
+
+/*
+ * Return true if the given string begins with "mix", except the 'm' can be anything, so "pix", "9ix" .. all count.
+ *
+ *
+ * mixStart("mix snacks") → true
+ * mixStart("pix snacks") → true
+ * mixStart("piz snacks") → false
+ */
+@Solution
+public boolean mixStart(String str) {
+    return str.indexOf("ix") == 1;
+}
+
+
+
+
+
+    @Inputs
+    public Object[] inputs() {
+        return new Object[] {
+            "mix snacks",
+            "pix snacks",
+            "piz snacks",
+            "nix",
+            "ni",
+            "n",
+            "",
+        };
+    }
+
+    @Results
+    public Object[] results() {
+        return new Object[] {
+            true,
+            true,
+            false,
+            true,
+            false,
+            false,
+            false,
+        };
+    }
+}
