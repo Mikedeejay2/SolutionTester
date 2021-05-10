2825962ee688985e46b8cbcfc33a86666be7d37d
Mikedeejay2 <58639173+Mikedeejay2@users.noreply.github.com>
2021-05-09 21:21:42 -0400

    Wrap Javadocs

--- src/com/mikedeejay2/codingbatsolutions/solutions/warmup1/In3050.java
+++ src/com/mikedeejay2/codingbatsolutions/solutions/warmup1/In3050.java
@@ -10,7 +10,8 @@ public class In3050 implements CodingBatSolution {
 
 
 /*
- * Given 2 int values, return true if they are both in the range 30..40 inclusive, or they are both in the range 40..50 inclusive.
+ * Given 2 int values, return true if they are both in the range 30..40 inclusive, or they are both in the range 40..50
+ * inclusive.
  *
  *
  * in3050(30, 31) → true
