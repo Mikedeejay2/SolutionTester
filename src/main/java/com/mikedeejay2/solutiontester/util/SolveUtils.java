7f2f9b74c32b6a9daecf4a9c84e7ca11d7643d9f
Mikedeejay2 <58639173+Mikedeejay2@users.noreply.github.com>
2021-07-08 15:15:25 -0400

    Remove unused SolveUtils methods

--- src/main/java/com/mikedeejay2/solutiontester/util/SolveUtils.java
+++ src/main/java/com/mikedeejay2/solutiontester/util/SolveUtils.java
@@ -1,23 +1,10 @@
 package com.mikedeejay2.solutiontester.util;
 
-import java.lang.reflect.Array;
 import java.util.Arrays;
 
 public final class SolveUtils {
     private SolveUtils() {
         throw new UnsupportedOperationException("This class should not be initialized.");
-    }
-
-    public static boolean isArray(Object obj) {
-        return obj instanceof Object[] ||
-            obj instanceof byte[] ||
-            obj instanceof short[] ||
-            obj instanceof int[] ||
-            obj instanceof long[] ||
-            obj instanceof char[] ||
-            obj instanceof float[] ||
-            obj instanceof double[] ||
-            obj instanceof boolean[];
     }
 
     public static boolean isInstance(Class<?> clazz1, Class<?> clazz2) {
@@ -38,54 +25,6 @@ public final class SolveUtils {
         Class<?> rootComponent2 = getRootComponent(clazz2);
         if(getArrayDepth(clazz1) != getArrayDepth(clazz2)) return false;
         return rootComponent1 == rootComponent2;
-    }
-
-    public static Class<?> toClassArray(Class<?> clazz) {
-        return Array.newInstance(clazz, 0).getClass();
-    }
-
-    public static Class<?> toPrimitiveClass(Class<?> clazz) {
-        try
-        {
-            return (Class<?>) clazz.getField("TYPE").get(null);
-        }
-        catch(NoSuchFieldException | IllegalAccessException ignored) {}
-        return clazz;
-    };
-
-    public static Object[] fixArr(Object[] obj) {
-        if(obj.length == 0) return obj;
-        Class<?>[] classes = new Class[obj.length];
-        for(int i = 0; i < obj.length; ++i) {
-            Object cur = obj[i];
-            if(cur == null) {
-                classes[i] = null;
-                continue;
-            }
-            classes[i] = cur.getClass();
-        }
-        Class<?> type = null;
-        for(int i = 0; i < classes.length; ++i) {
-            Class<?> cur = classes[i];
-            if(cur == null) continue;
-            if(type == null) {
-                type = cur;
-            }
-            if(type != cur) {
-                throw new IllegalArgumentException(
-                    String.format("%s is not of type %s",
-                                  cur.getSimpleName(), type.getSimpleName()));
-            }
-        }
-        Object[] newArr = (Object[]) Array.newInstance(type, obj.length);
-        System.arraycopy(obj, 0, newArr, 0, obj.length);
-        return newArr;
-    };
-
-    public static Object[] classArrCast(Class<?> clazz, Object[] obj) {
-        Object newArr = Array.newInstance(clazz, obj.length);
-        System.arraycopy(obj, 0, newArr, 0, obj.length);
-        return (Object[]) newArr;
     }
 
     public static Class<?> getRootComponent(Class<?> clazz) {
