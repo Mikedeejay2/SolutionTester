703716e39fc6e103d056185cb85dc739b5336f07
Mikedeejay2 <58639173+Mikedeejay2@users.noreply.github.com>
2021-07-10 18:19:36 -0400

    Javadocs

--- src/main/java/com/mikedeejay2/solutiontester/test/SolutionTester.java
+++ src/main/java/com/mikedeejay2/solutiontester/test/SolutionTester.java
@@ -8,17 +8,41 @@ import org.jetbrains.annotations.Nullable;
 import java.util.concurrent.CompletableFuture;
 import java.util.function.Function;
 
+/**
+ * Tester for {@link SolutionTest} tests. Utilizes {@link SolutionTestSolver} and {@link SolutionPrinter} to test and
+ * print results.
+ * <p>
+ * Use {@link SolutionTester#apply(SolutionTest)} for
+ * inputting a <code>SolutionTest</code>.
+ */
 public class SolutionTester implements Function<SolutionTest, CompletableFuture<TestResults>> {
-    protected SolutionPrinter printer;
+    /**
+     * The {@link SolutionPrinter} to use. Can be null if not needed.
+     */
+    protected @Nullable SolutionPrinter printer;
 
+    /**
+     * Construct a new <code>SolutionTester</code> with the default <code>PrintStream</code> of {@link System#out}
+     */
     public SolutionTester() {
         this.printer = new SolutionPrinter(System.out::println);
     }
 
+    /**
+     * Construct a new <code>SolutionTester</code> with a {@link SolutionPrinter} instance
+     *
+     * @param printer The {@link SolutionPrinter} to use. Nullable.
+     */
     public SolutionTester(@Nullable SolutionPrinter printer) {
         this.printer = printer;
     }
 
+    /**
+     * Solve a {@link SolutionTest}
+     *
+     * @param solutionTest The <code>SolutionTest</code> to solve
+     * @return The {@link CompletableFuture} of the {@link TestResults}
+     */
     private CompletableFuture<TestResults> solve(@NotNull final SolutionTest solutionTest) {
         final CompletableFuture<TestResults> testFuture = CompletableFuture.supplyAsync(solutionTest._toSolver());
         if(printer != null) testFuture.thenAccept(printer);
@@ -26,6 +50,12 @@ public class SolutionTester implements Function<SolutionTest, CompletableFuture<
         return testFuture;
     }
 
+    /**
+     * Solve a {@link SolutionTest}
+     *
+     * @param solutionTest The <code>SolutionTest</code> to solve
+     * @return The {@link CompletableFuture} of the {@link TestResults}
+     */
     @Override
     public CompletableFuture<TestResults> apply(@NotNull final SolutionTest solutionTest) {
         try {
@@ -36,16 +66,31 @@ public class SolutionTester implements Function<SolutionTest, CompletableFuture<
         }
     }
 
-    public SolutionPrinter getPrinter() {
+    /**
+     * Get the {@link SolutionPrinter} for this tester. Can be null.
+     *
+     * @return The <code>SolutionPrinter</code>
+     */
+    public @Nullable SolutionPrinter getPrinter() {
         return printer;
     }
 
-    public void setPrinter(SolutionPrinter printer) {
+    /**
+     * Set the {@link SolutionPrinter} for this tester. Can be null.
+     *
+     * @param printer The new <code>SolutionPrinter</code>
+     */
+    public void setPrinter(@Nullable SolutionPrinter printer) {
         this.printer = printer;
     }
 
+    /**
+     * Gets result String if all processes have been completed
+     *
+     * @return Result String given that a printer exists and operations have finished
+     */
     @Override
     public String toString() {
-        return printer.toString();
+        return printer != null ? printer.toString() : super.toString();
     }
 }
